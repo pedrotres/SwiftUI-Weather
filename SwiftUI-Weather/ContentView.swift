5b3786d93dfe57c935c0f4e0831217c46dc19270
Pedro Francisco Tres <52545592+pedrotres@users.noreply.github.com>
2023-11-14 22:35:41 -0300

    add weatherButton

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -42,16 +42,17 @@ struct ContentView: View {
                         temperature: 25
                     )
                 }
+                
                 Spacer()
                 
                 Button {
                     print("Tapped")
                 } label: {
-                   WeatherButton(
-                    title: "Change day time",
-                    textColor: .blue,
-                    backgroundColor: .white
-                   )
+                    Text("Change day time")
+                        .frame(width: 280, height: 50)
+                        .background(Color.white)
+                        .font(.system(size: 20, weight: .bold, design: .default))
+                        .cornerRadius(10)
                 }
                 
                 Spacer()
@@ -134,21 +135,5 @@ struct MainWeatherStatusView: View {
                     .padding(.bottom, 40)
             }
         }
-    }
-}
-
-struct WeatherButton: View {
-    
-    var title: String
-    var textColor: Color
-    var backgroundColor: Color
-    
-    var body: some View {
-        Text(title)
-            .frame(width: 280, height: 50)
-            .background(backgroundColor)
-            .foregroundStyle(textColor)
-            .font(.system(size: 20, weight: .bold, design: .default))
-            .clipShape(.buttonBorder, style: FillStyle())
     }
 }
